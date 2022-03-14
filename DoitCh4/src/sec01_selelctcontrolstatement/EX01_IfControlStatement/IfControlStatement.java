8e10cb763e46f02331f02e160e035d47cdee3948
Mingu Kim <mingu0429@naver.com>
2022-03-14 10:26:55 +0900

    Second commit

---
+++ DoitCh4/src/sec01_selelctcontrolstatement/EX01_IfControlStatement/IfControlStatement.java
@@ -0,0 +1,17 @@
+package sec01_selelctcontrolstatement.EX01_IfControlStatement;
+
+public class IfControlStatement {
+	public static void main(String[] args) {
+
+		int value1 = 5;
+		if (value1 > 3) {
+			System.out.println("실행1");
+		}
+		if (value1 < 5) {
+			System.out.println("실행2");
+		}
+
+		// 하단 필요없어서 작성 안했음
+	}
+
+}
