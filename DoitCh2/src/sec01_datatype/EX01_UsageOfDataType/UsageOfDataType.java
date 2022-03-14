a4c3e0a2365b7614f9318bbdef3a92d659ee0bf4
Mingu Kim <mingu0429@naver.com>
2022-03-14 10:23:26 +0900

    First commit

---
+++ DoitCh2/src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
@@ -0,0 +1,13 @@
+package sec01_datatype.EX01_UsageOfDataType;
+
+public class UsageOfDataType {
+	public static void main(String[] args) {
+
+		int a = 3;
+
+		int b;
+		b = 4;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
