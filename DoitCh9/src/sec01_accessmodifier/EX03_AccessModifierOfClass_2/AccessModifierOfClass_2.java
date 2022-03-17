feda899b85748e84d2a38df0f78040e5fa9bd9ab
Mingu Kim <mingu0429@naver.com>
2022-03-17 12:38:12 +0900

    DoitCh9

---
+++ DoitCh9/src/sec01_accessmodifier/EX03_AccessModifierOfClass_2/AccessModifierOfClass_2.java
@@ -0,0 +1,12 @@
+package sec01_accessmodifier.EX03_AccessModifierOfClass_2;
+
+import sec01_accessmodifier.EX03_AccessModifierOfClass_2.pack.AA;
+import sec01_accessmodifier.EX03_AccessModifierOfClass_2.pack.CC;
+
+public class AccessModifierOfClass_2 {
+	public static void main(String[] args) {
+		AA a = new AA();
+
+	}
+
+}
