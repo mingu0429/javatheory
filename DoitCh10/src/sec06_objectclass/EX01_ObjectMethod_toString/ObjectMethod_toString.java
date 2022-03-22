0c710db52baba4963ac817fa5b737b2e4b902d42
Mingu Kim <mingu0429@naver.com>
2022-03-22 15:24:24 +0900

    DoitCh10

---
+++ DoitCh10/src/sec06_objectclass/EX01_ObjectMethod_toString/ObjectMethod_toString.java
@@ -0,0 +1,27 @@
+package sec06_objectclass.EX01_ObjectMethod_toString;
+
+class A {
+	int a = 3;
+	int b = 4;
+}
+
+class B {
+	int a = 3;
+	int b = 4;
+
+	public String toString() {
+		return "필드값(a, b) = " + a + " " + b;
+	}
+}
+
+public class ObjectMethod_toString {
+	public static void main(String[] args) {
+		A a = new A();
+		B b = new B();
+
+		System.out.printf("%x\n", a.hashCode()); //hashcode를 16진수로 표현
+		System.out.println(a.toString());
+		System.out.println(b);
+	}
+
+}
---
+++ DoitCh10/src/shortkey/ShortKey.java
@@ -0,0 +1,12 @@
+package shortkey;
+
+public class ShortKey {
+	/*System.out.println();
+	System.out.println();
+	System.out.println();
+	System.out.println();
+//ctrl art shift down 행단위 복사	
+	System.out.println();
+//art 화살표 위 아래 행단위 움직임
+	//*/
+}
