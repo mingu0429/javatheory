e6ea9f7e0d5805283eff08deb10a6762efc58fa2
Mingu Kim <mingu0429@naver.com>
2022-04-01 22:17:35 +0900

    Create Ex01.java

---
+++ ObjectOrientedProgramming/src/ex01/Ex01.java
@@ -0,0 +1,16 @@
+package ex01;
+public class Ex01 {
+    public static void display(int[] array) {
+	for (int i = 0; i < array.length; i++) {
+	      System.out.print(array[i] + " ");
+	}
+	System.out.println();
+    }
+
+    public static void display(String[] array) {
+          for (int i = 0; i < array.length; i++) {
+	      System.out.print(array[i] + " ");
+          }
+	System.out.println();
+    }
+}
